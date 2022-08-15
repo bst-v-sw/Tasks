4c2034b0bdd70807578a89b748eceefb2a883bcf
Vadim <110719459+bst-v-sw@users.noreply.github.com>
2022-08-15 08:17:45 +0300

    git commit -m 'Task1'

---
+++ Task1.js
@@ -0,0 +1,5 @@
+//Выведем на экран первую и последнюю буквы предложения, записанного в константу text, в следующем формате:
+// First: N
+// Last : t
+const text = 'Never forget what you are, for surely the world will not';
+console.log(`First: ${text[0]}\nLast: ${text[length(text) - 1]}`);
