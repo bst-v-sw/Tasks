385bb2348fc900da7abb52ab9049ac19024a93c4
Vadim <110719459+bst-v-sw@users.noreply.github.com>
2022-08-17 08:24:55 +0300

    fix array_1.js

--- array_1.js
+++ array_1.js
@@ -1,6 +1,11 @@
 // Массивы
 // Практика с даннами массивов
 const cars = ['Мазда','Форд','Мерседес','БМВ']
+const people = [
+    {name: 'Vadim' , budget: 4200},
+    {name: 'Victoria' , budget: 3500},
+    {name: 'Olga' , budget: 1700},
+]
 const fib = [1, 1, 2, 3, 5, 8, 13]
 //
 //console.log(cars.length)
@@ -24,10 +29,30 @@ const fib = [1, 1, 2, 3, 5, 8, 13]
 // console.log(cars.reverse())
 // console.log(cars)
 
-//Задача 1
-//Разделить строку по буквам через split, выполнить реверс строки и превратить в новую строку путем соединения элементов
-const text ='Привет, мы изучаем JavaScript'
-const reverseText = text.split('').reverse().join(' ')
-console.log(reverseText)
+//Возврат индекса по элементу
+// const index = cars.indexOf('БМВ')
+// console.log(index)
+// cars[index] = 'Porshe'
+// console.log(cars)
+
+// Поиск индекса по элементу
+const index = people.findIndex(function (person) {
+    return person.budget === 3500
+})
+console.log(index)
+//Поиск Элемента
+//упрощенная запись - через стрелочную функцию
+const person = people.find(person => person.budget === 3500)
+    console.log(person)
+
+
+
+// //Задача 1
+// //Разделить строку по буквам через split, выполнить реверс строки и превратить в новую строку путем соединения элементов
+// const text ='Привет, мы изучаем JavaScript'
+// const reverseText = text.split('').reverse().join(' ')
+// console.log(reverseText)
+
+
 
 
