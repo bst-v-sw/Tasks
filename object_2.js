b1d18174c6f1a0a95bba627071300b2c37d71d6d
Vadim <110719459+bst-v-sw@users.noreply.github.com>
2022-08-19 07:48:22 +0300

    fix object_2

--- object_2.js
+++ object_2.js
@@ -1,33 +1,46 @@
-//Объекты
-//Получение свойств объекта
-const myCity = {
-    city: 'New York',
-    popular: true ,
-    country : 'USA'
-}
+// //Объекты
+// //Получение свойств объекта
+// const myCity = {
+//     city: 'New York',
+//     popular: true ,
+//     country : 'USA'
+// }
+//
+// console.log(myCity.city) // 'New York'
+// console.log(myCity.popular)// 'true'
+//
+// // Изменение (добавление) свойств объекта
+// const myCity = {
+//     city: 'New York',
+// }
+// myCity.popular = true
+// console.log(myCity)
+// myCity.country = 'USA'
+// console.log(myCity)
+//
+// // Вложенные свойства объектов
+// const myCity = {
+//     city: 'New York',
+//     info: {
+//         isPopular : true,
+//         country : 'USA'
+//     }
+// }
+// console.log(myCity.info.isPopular)
+// //Удаление свойства
+// delete myCity.info['isPopular']
+// console.log(myCity)
 
-console.log(myCity.city) // 'New York'
-console.log(myCity.popular)// 'true'
-
-// Изменение (добавление) свойств объекта
-const myCity = {
-    city: 'New York',
+//Деструктуризация объектов
+const userProfile = {
+    name: 'VADIM' ,
+    commentsQty: 25 ,
+    hasSignedAgreement: false ,
 }
-myCity.popular = true
-console.log(myCity)
-myCity.country = 'USA'
-console.log(myCity)
+const { name, commentsQty } = userProfile
+const { hasSignedAgreement } = userProfile
 
-// Вложенные свойства объектов
-const myCity = {
-    city: 'New York',
-    info: {
-        isPopular : true,
-        country : 'USA'
-    }
-}
-console.log(myCity.info.isPopular)
-//Удаление свойства
-delete myCity.info['isPopular']
-console.log(myCity)
+console.log(name)
+console.log(commentsQty)
+console.log(hasSignedAgreement)
 
