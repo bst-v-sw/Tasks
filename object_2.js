89cde3d7f8f5797ef21bcfa826cc10755efe75ca
Vadim <110719459+bst-v-sw@users.noreply.github.com>
2022-08-20 09:31:39 +0300

    fix object_2

--- object_2.js
+++ object_2.js
@@ -1,35 +1,35 @@
-// //Объекты
-// //Получение свойств объекта
-// const myCity = {
-//     city: 'New York',
-//     popular: true ,
-//     country : 'USA'
-// }
-//
-// console.log(myCity.city) // 'New York'
-// console.log(myCity.popular)// 'true'
-//
-// // Изменение (добавление) свойств объекта
-// const myCity = {
-//     city: 'New York',
-// }
-// myCity.popular = true
-// console.log(myCity)
-// myCity.country = 'USA'
-// console.log(myCity)
-//
-// // Вложенные свойства объектов
-// const myCity = {
-//     city: 'New York',
-//     info: {
-//         isPopular : true,
-//         country : 'USA'
-//     }
-// }
-// console.log(myCity.info.isPopular)
-// //Удаление свойства
-// delete myCity.info['isPopular']
-// console.log(myCity)
+//Объекты
+//Получение свойств объекта
+const myCity = {
+    city: 'New York',
+    popular: true ,
+    country : 'USA'
+}
+
+console.log(myCity.city) // 'New York'
+console.log(myCity.popular)// 'true'
+
+// Изменение (добавление) свойств объекта
+const myCity = {
+    city: 'New York',
+}
+myCity.popular = true
+console.log(myCity)
+myCity.country = 'USA'
+console.log(myCity)
+
+// Вложенные свойства объектов
+const myCity = {
+    city: 'New York',
+    info: {
+        isPopular : true,
+        country : 'USA'
+    }
+}
+console.log(myCity.info.isPopular)
+//Удаление свойства
+delete myCity.info['isPopular']
+console.log(myCity)
 
 //Деструктуризация объектов
 const userProfile = {
@@ -44,3 +44,21 @@ console.log(name)
 console.log(commentsQty)
 console.log(hasSignedAgreement)
 
+// Циклы for in для объектов
+const myObject ={
+    x: 10,
+    y: true,
+    z: 'abc'
+}
+for (const key in myObject) {
+    console.log(key, myObject[key])
+}
+//forEach для объектов
+const myObject ={
+    x: 10,
+    y: true,
+    z: 'abc'
+}
+Object.keys(myObject).forEach(key => {
+    console.log(key, myObject[key])
+})
