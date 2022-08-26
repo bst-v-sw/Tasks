31cb81a20290ec51de844d9564d4449cfd964e60
Vadim <110719459+bst-v-sw@users.noreply.github.com>
2022-08-26 10:16:09 +0300

    classes inheritacne

--- classes_1.js
+++ classes_1.js
@@ -25,3 +25,39 @@ let rabbit = new Rabbit("Белый кролик");
 
 rabbit.run(5); // Белый кролик бежит со скоростью 5.
 rabbit.hide(); // Белый кролик прячется!
+
+//===============================
+class Animal {
+
+    constructor(name) {
+        this.speed = 0;
+        this.name = name;
+    }
+
+    run(speed) {
+        this.speed = speed;
+        console.log(`${this.name} бежит со скоростью ${this.speed}.`);
+    }
+
+    stop() {
+        this.speed = 0;
+        console.log(`${this.name} стоит.`);
+    }
+
+}
+
+class Rabbit extends Animal {
+    hide() {
+        console.log(`${this.name} прячется!`);
+    }
+
+    stop() {
+        super.stop(); // вызываем родительский метод stop
+        this.hide(); // и затем hide
+    }
+}
+
+let rabbit = new Rabbit("Белый кролик");
+
+rabbit.run(5); // Белый кролик бежит со скоростью 5.
+rabbit.stop(); // Белый кролик стоит. Белый кролик прячется!
