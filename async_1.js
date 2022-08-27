025a12623fcd660cdc449eaef061632a2f435717
Vadim <110719459+bst-v-sw@users.noreply.github.com>
2022-08-27 16:35:48 +0300

    fix classes_1.js

--- async_1.js
+++ async_1.js
@@ -1,54 +1,54 @@
 // //Event Loop
+
+const timeout = setTimeout(() => {
+    clearTimeout(timeout)
+    console.log('After timeout')
+}, 2500)
 //
-// const timeout = setTimeout(() => {
-//     clearTimeout(timeout)
-//     console.log('After timeout')
-// }, 2500)
-//
-// clearTimeout(timeout)
-//
-// const interval = setInterval(() => {
-//     console.log('After timeout')
-// }, 1000)
-//
-// clearTimeout(interval)
-//
-// const delay = (callback, wait = 1000) => {
-//     setTimeout(callback, wait)
-// }
-// delay (() => {
-//     console.log('After 2 seconds')
-// }, 2000)
-//
-// const delay = (wait = 1000) =>{
-//     const promise = new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve()
-//         }, wait)
-//     })
-//     return promise
-// }
-//
-// delay(2500)
-//     .then(() => {
-//         console.log('After 2 seconds')
-//     })
-//     .catch(err => console.error(err))
-//     .finally(() => console.log('Finally'))
+clearTimeout(timeout)
+
+const interval = setInterval(() => {
+    console.log('After timeout')
+}, 1000)
+
+clearTimeout(interval)
+
+const delay = (callback, wait = 1000) => {
+    setTimeout(callback, wait)
+}
+delay (() => {
+    console.log('After 2 seconds')
+}, 2000)
+
+const delay = (wait = 1000) =>{
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, wait)
+    })
+    return promise
+}
+
+delay(2500)
+    .then(() => {
+        console.log('After 2 seconds')
+    })
+    .catch(err => console.error(err))
+    .finally(() => console.log('Finally'))
 
 // Промис при успешном выполнении
-// let promise = new Promise(function(resolve, reject) {
-//     // эта функция выполнится автоматически, при вызове new Promise
-//
-//     // через 1 секунду сигнализировать, что задача выполнена с результатом "done"
-//     setTimeout(() => resolve('done'), 1000);
-// })
-//
-// //Промисы при ошибке
-// let promise = new Promise(function(resolve, reject) {
-//     // спустя одну секунду будет сообщено, что задача выполнена с ошибкой
-//     setTimeout(() => reject(new Error("Whoops!")), 1000);
-// });
+let promise = new Promise(function(resolve, reject) {
+    // эта функция выполнится автоматически, при вызове new Promise
+
+    // через 1 секунду сигнализировать, что задача выполнена с результатом "done"
+    setTimeout(() => resolve('done'), 1000);
+})
+
+//Промисы при ошибке
+let promise = new Promise(function(resolve, reject) {
+    // спустя одну секунду будет сообщено, что задача выполнена с ошибкой
+    setTimeout(() => reject(new Error("Whoops!")), 1000);
+});
 
 // Использование async/await
 async function f() {
--- classes_1.js
+++ classes_1.js
@@ -1,63 +1,70 @@
 //Наследование классов
-class Animal {
-    constructor(name) {
-        this.speed = 0;
-        this.name = name;
-    }
-    run(speed) {
-        this.speed = speed;
-        console.log(`${this.name} бежит со скоростью ${this.speed}.`);
-    }
-    stop() {
-        this.speed = 0;
-        console.log(`${this.name} стоит.`);
-    }
-}
+// class Animal {
+//     constructor(name) {
+//         this.speed = 0;
+//         this.name = name;
+//     }
+//     run(speed) {
+//         this.speed = speed;
+//         console.log(`${this.name} бежит со скоростью ${this.speed}.`);
+//     }
+//     stop() {
+//         this.speed = 0;
+//         console.log(`${this.name} стоит.`);
+//     }
+// }
+//
+// // Наследуем от Animal указывая "extends Animal"
+// class Rabbit extends Animal {
+//     hide() {
+//         console.log(`${this.name} прячется!`);
+//     }
+// }
+//
+// let rabbit = new Rabbit("Белый кролик");
+//
+// rabbit.run(5); // Белый кролик бежит со скоростью 5.
+// rabbit.hide(); // Белый кролик прячется!
+//
+// //===============================
+// class Animal {
+//
+//     constructor(name) {
+//         this.speed = 0;
+//         this.name = name;
+//     }
+//
+//     run(speed) {
+//         this.speed = speed;
+//         console.log(`${this.name} бежит со скоростью ${this.speed}.`);
+//     }
+//
+//     stop() {
+//         this.speed = 0;
+//         console.log(`${this.name} стоит.`);
+//     }
+//
+// }
+//
+// class Rabbit extends Animal {
+//     hide() {
+//         console.log(`${this.name} прячется!`);
+//     }
+//
+//     stop() {
+//         super.stop(); // вызываем родительский метод stop
+//         this.hide(); // и затем hide
+//     }
+// }
+//
+// let rabbit = new Rabbit("Белый кролик");
+//
+// rabbit.run(5); // Белый кролик бежит со скоростью 5.
+// rabbit.stop(); // Белый кролик стоит. Белый кролик прячется!
 
-// Наследуем от Animal указывая "extends Animal"
-class Rabbit extends Animal {
-    hide() {
-        console.log(`${this.name} прячется!`);
-    }
-}
+//========================================
+class Rabbit {}
+let rabbit = new Rabbit();
 
-let rabbit = new Rabbit("Белый кролик");
-
-rabbit.run(5); // Белый кролик бежит со скоростью 5.
-rabbit.hide(); // Белый кролик прячется!
-
-//===============================
-class Animal {
-
-    constructor(name) {
-        this.speed = 0;
-        this.name = name;
-    }
-
-    run(speed) {
-        this.speed = speed;
-        console.log(`${this.name} бежит со скоростью ${this.speed}.`);
-    }
-
-    stop() {
-        this.speed = 0;
-        console.log(`${this.name} стоит.`);
-    }
-
-}
-
-class Rabbit extends Animal {
-    hide() {
-        console.log(`${this.name} прячется!`);
-    }
-
-    stop() {
-        super.stop(); // вызываем родительский метод stop
-        this.hide(); // и затем hide
-    }
-}
-
-let rabbit = new Rabbit("Белый кролик");
-
-rabbit.run(5); // Белый кролик бежит со скоростью 5.
-rabbit.stop(); // Белый кролик стоит. Белый кролик прячется!
+// это объект класса Rabbit?
+console.log( rabbit instanceof Rabbit ); // true
